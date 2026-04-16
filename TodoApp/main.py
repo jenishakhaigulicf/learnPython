4b4574f122a9d683686101c2e13bb3d65532f72d
jenisha khaiguli <jenisha.khaiguli@eclathealth.com>
2026-04-16 13:14:48 +0545

    feat: add admin and user routes

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,13 @@
+from fastapi import FastAPI
+
 import models
 from database import engine
-from fastapi import FastAPI
-from routers import auth, todos
+from routers import admin, auth, todos, user
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(user.router)
